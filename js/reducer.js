0675b52524df42459be60596907b62fb446e456a
cassymarie <cassy2205@gmail.com>
2021-03-09 16:57:26 -0700

    commit tree 97320fcd422ebdcbe52ba585ac8a631acd46cabf

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,35 @@
-// add code snippets from README
+// Start by remembering our core fact about how redux works
+//  ACTION (DISPATCH) >> REDUCER >> STATE (CHANGED BY REDUCER)
+let state 
+
+//Use a default argument in the reducer to set the initial state
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+//Wrap the execution of our reducer in a function that we call dispatch
+function dispatch(action){
+    state = reducer(state, action)
+    render()
+}
+
+//Use the render function to display our state
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  } 
+
+  
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    dispatch({type: 'INCREASE_COUNT'})
+}) 
+
+
+dispatch({type: '@@INIT'})
